f0fa71075c0efdf6df5e2be8083b135a8b0c5e7d
Nikita Khrulev <nikita.khrulev00@gmail.com>
2024-11-22 12:23:07 +0300

    some changes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
-import './utils';
+import { createMockPosts } from './posts';
 
+createMockPosts();
--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,3 @@
-import { postsData } from './data';
-
-const { POSTS_COUNT, names, descriptions, comments} = postsData;
-
 function getRandomInteger (min, max) {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
@@ -26,24 +22,4 @@ function createRandomId (min, max) {
   };
 }
 
-const generatePhotoId = createRandomId(1, POSTS_COUNT);
-const getRandomCommentId = createRandomId(1, 30 * POSTS_COUNT);
-
-const createComments = () => Array.from({length: getRandomInteger(0, 30)}, () => ({
-  id: getRandomCommentId(),
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: comments[getRandomInteger(0, comments.length - 1)],
-  name: names[getRandomInteger(0, names.length - 1)]
-}));
-
-const data = Array.from({length: POSTS_COUNT}, () => {
-  const id = generatePhotoId();
-  return {
-    id: id,
-    url: `photos/${id}.jpg`,
-    description: descriptions[getRandomInteger(0, descriptions.length - 1)],
-    likes: getRandomInteger(15, 200),
-    comments: createComments(),
-  };
-});
-console.log(data);
+export {createRandomId, getRandomInteger};
