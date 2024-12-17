2ceaacd5ff1011e8ee1e28f29b2c8510b5e27998
Keks <keks@htmlacademy.ru>
2024-12-17 16:39:51 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,15 @@
+import { createMockPosts } from './mock.js';
+import { printThumbnails } from './thumbnails.js';
+import { picturesContainer } from './thumbnails.js';
+import { findPostContent } from './post.js';
+
+const initApp = () => {
+  const data = createMockPosts();
+  printThumbnails(data);
+  picturesContainer.addEventListener('click', (evt) => {
+    findPostContent(evt, data);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
+
---
+++ 10/js/post.js
@@ -0,0 +1,86 @@
+import { escKeypress } from './utils';
+
+const postModal = document.querySelector('.big-picture');
+const closeModalBtn = document.querySelector('.big-picture__cancel');
+const postComments = document.querySelector('.social__comments');
+const loadMore = document.querySelector('.social__comments-loader');
+const commentsCount = document.querySelector('.social__comment-shown-count');
+const commentsTotal = document.querySelector('.social__comment-total-count');
+
+const COUNT_STEP = 5;
+let currentCount = 0;
+let comments = [];
+
+const closePostModal = () => {
+  postModal.classList.add('hidden');
+  closeModalBtn.removeEventListener('click', closePostModal);
+  document.body.classList.remove('modal-open');
+};
+
+const clearComments = () => {
+  currentCount = 0;
+  postComments.innerHTML = '';
+  loadMore.classList.remove('hidden');
+};
+
+const checkCommentsLength = () => {
+  commentsCount.textContent = currentCount;
+  if (commentsCount.textContent === commentsTotal.textContent) {
+    loadMore.classList.add('hidden');
+  }
+};
+
+const insertMoreComments = (arr) => {
+  const commentsFragment = document.createDocumentFragment();
+  const commentsToPrint = arr.slice(currentCount, currentCount + COUNT_STEP);
+  currentCount += commentsToPrint.length;
+
+  commentsToPrint.forEach((comment) => {
+    const commentNode = document.createElement('li');
+    commentNode.classList.add('social__comment');
+    commentNode.innerHTML = `
+    <img
+      class="social__picture"
+      src="${comment.avatar}"
+      alt="${comment.name}"
+      width="35" height="35">
+    <p class="social__text">${comment.message}</p>`;
+    commentsFragment.append(commentNode);
+  });
+
+  postComments.appendChild(commentsFragment);
+  checkCommentsLength();
+};
+
+const insertPostComments = (newComments) => {
+  comments = newComments;
+  clearComments();
+  insertMoreComments(comments);
+};
+
+const openPostModal = (content) => {
+  postModal.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  postModal.querySelector('img').src = content.url;
+  postModal.querySelector('.likes-count').textContent = content.likes;
+  postModal.querySelector('.social__caption').textContent = content.description;
+  postModal.querySelector('.social__comment-total-count').textContent = content.comments.length;
+  clearComments();
+  insertPostComments(content.comments);
+  closeModalBtn.addEventListener('click', closePostModal);
+};
+
+const findPostContent = (evt, data) => {
+  const url = evt.target.closest('.picture').querySelector('img').src;
+  const photoId = Number(url.split('/').pop().split('.')[0]);
+  const postData = data.find((photo) => photo.id === photoId);
+  openPostModal(postData);
+};
+
+document.addEventListener('keydown', (evt) => {
+  escKeypress(evt, closePostModal);
+});
+
+loadMore.addEventListener('click', () => insertMoreComments(comments));
+
+export {findPostContent, postComments};
