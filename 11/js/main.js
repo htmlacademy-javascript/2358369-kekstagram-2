cbff74cc568417f903488fbb88c866359213ae46
Keks <keks@htmlacademy.ru>
2024-12-27 00:04:22 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,17 @@
+import { createMockPosts } from './mock.js';
+import { printThumbnails } from './thumbnails.js';
+import { picturesContainer } from './thumbnails.js';
+import { findPostContent } from './post.js';
+import './form.js';
+import './validation.js';
+
+const initApp = () => {
+  const data = createMockPosts();
+  printThumbnails(data);
+  picturesContainer.addEventListener('click', (evt) => {
+    findPostContent(evt, data);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
+
---
+++ 11/js/utils.js
@@ -0,0 +1,31 @@
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const createRandomId = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      throw new Error('Уникальные значения исчерпаны');
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const escKeypress = (evt, fn) => {
+  if (evt.key === 'Escape') {
+    fn();
+  }
+};
+
+export {createRandomId, getRandomInteger, escKeypress};
